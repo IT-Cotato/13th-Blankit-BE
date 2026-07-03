21761cc5332244fda52e2cdbf654c690e9bd5e8d
김서윤 <sy0518@gimseoyun-ui-MacBookAir.local>
2026-07-04 00:37:39 +0900

    chore: 깃허브 협업 환경 설정

---
+++ src/main/java/com/cotato/blankit/BlankitApplication.java
@@ -0,0 +1,13 @@
+package com.cotato.blankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankitApplication.class, args);
+	}
+
+}
